cd22844fdc0ebc0222b8f3a6d8d255ad77f8e448
KIRILL EROPKIN <krill221@gmail.com>
2023-09-16 19:10:51 +0300

    added: title (#1)
    * added: title
    
    * name added
    
    ---------
    
    Co-authored-by: Еропкин Кирилл <krill2211@yandex.ru>

--- frontend/index.ts
+++ frontend/index.ts
@@ -3,7 +3,7 @@ import figlet from "figlet";
 const server = Bun.serve({
   fetch() {
     const body = figlet.textSync("Bun!!!");
-    return new Response(body);
+    return new Response("title Name \n" + body);
   },
   port: 3000,
 });
