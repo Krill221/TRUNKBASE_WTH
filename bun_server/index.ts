4cc348e08502f4c43be5d37e7e2d05a9994b429a
KIRILL EROPKIN <krill221@gmail.com>
2023-09-21 23:16:42 +0300

    added test

--- bun_server/index.ts
+++ bun_server/index.ts
@@ -1,6 +1,6 @@
 import figlet from "figlet";
 
-const server = Bun.serve({
+Bun.serve({
   fetch() {
     const body = figlet.textSync("Bun featMain1 Bun dev dev dev!!!");
     return new Response("title Name \n" + body);
