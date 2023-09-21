4c24112ec26782833ac4fb21da10e924500662e8
KIRILL EROPKIN <krill221@gmail.com>
2023-09-21 21:48:00 +0300

    Feat main1 (#5)
    * last 1
    
    * added: text

--- frontend/index.ts
+++ frontend/index.ts
@@ -2,7 +2,7 @@ import figlet from "figlet";
 
 const server = Bun.serve({
   fetch() {
-    const body = figlet.textSync("Bun feat 1 sunshine !!!");
+    const body = figlet.textSync("Bun featMain1");
     return new Response("title Name \n" + body);
   },
   port: 3000,
