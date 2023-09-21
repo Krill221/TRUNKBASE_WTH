632926b67faa227b70c8fc78c3f9ea96137cc767
dev1 <dev1tbd@yandex.ru>
2023-09-21 18:49:38 +0000

    added:dev dev dev

--- frontend/index.ts
+++ frontend/index.ts
@@ -2,7 +2,7 @@ import figlet from "figlet";
 
 const server = Bun.serve({
   fetch() {
-    const body = figlet.textSync("Bun!!!");
+    const body = figlet.textSync("Bun dev dev dev!!!");
     return new Response("title Name \n" + body);
   },
   port: 3000,
