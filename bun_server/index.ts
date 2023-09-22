4771520e4344dc769cf1d13f4d5f621cbf8bed0f
KIRILL EROPKIN <krill221@gmail.com>
2023-09-22 18:34:44 +0300

    bdd

--- bun_server/index.ts
+++ bun_server/index.ts
@@ -1,11 +1,33 @@
-import figlet from "figlet";
+import { getFlags } from "./flags";
+import * as amplitude from "@amplitude/analytics-node";
 
-Bun.serve({
-  fetch() {
-    const body = figlet.textSync("Bun featMain1 Bun dev dev dev!!!");
-    return new Response("title Name \n" + body);
-  },
-  port: 3000,
+amplitude.init("50231d51dc0981810df71ea2bce095c9", {
+  logLevel: amplitude.Types.LogLevel.Warn,
 });
 
-const s = 1;
+async function bootstrap() {
+  const experiment = await getFlags();
+
+  Bun.serve({
+    async fetch() {
+      let body = "";
+
+      const user = { device_id: "id12123" };
+      const flags = await experiment.evaluate(user);
+      console.log(flags);
+
+      if (flags["header_1_feature_flag"]?.value === "on") {
+        body += "<h1>HEADER_1_FEATURE_FLAG<h1>";
+      }
+      body += "<div>Bun featMain1 Bun dev dev dev!!!<div>";
+
+      return new Response(body, {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
+    },
+    port: 3000,
+  });
+}
+bootstrap();
