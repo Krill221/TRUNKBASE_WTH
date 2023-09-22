3457e6022831d77657a1a4aafc87fab0cf39f9c2
KIRILL EROPKIN <krill221@gmail.com>
2023-09-22 19:42:21 +0300

    Action1 (#7)
    * action 1 added
    
    * action 1 added 2
    
    * action 1 added 3
    
    * action 1 added 3
    
    * added test
    
    * added test
    
    * p
    
    * p
    
    * bdd
    
    * bdd
    
    * bdd
    
    * bdd
    
    * bdd
    
    * bdd
    
    * bdd2
    
    * bdd3
    
    * bdd3
    
    * bdd3

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # TRUNKBASE WORKTHROUGH
+
+
+# dockerfile
+FROM ubuntu:latest
+RUN apt-get -y update
+RUN apt-get -y install git
+# run dev
--- frontend/index.ts
+++
@@ -1,9 +0,0 @@
-import figlet from "figlet";
-
-const server = Bun.serve({
-  fetch() {
-    const body = figlet.textSync("Bun featMain1 Bun dev dev dev!!!");
-    return new Response("title Name \n" + body);
-  },
-  port: 3000,
-});
